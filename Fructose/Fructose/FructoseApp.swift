47ad3f22e93eec15c3a45d187b8ce10ddb89d29d
iwy2th <huyloind03@gmail.com>
2023-06-03 11:31:10 +0700

    commit test

---
+++ Fructose/Fructose/FructoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructoseApp.swift
+//  Fructose
+//
+//  Created by Iwy2th on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FructoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
